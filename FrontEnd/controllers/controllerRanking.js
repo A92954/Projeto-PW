a1fc672b6c0ba27df20072598b74149e2d364ebd
A92954 <a92954@alunos.uminho.pt>
2021-01-18 23:02:15 +0000

    ranking dataTable

--- FrontEnd/controllers/controllerRanking.js
+++ FrontEnd/controllers/controllerRanking.js
@@ -4,59 +4,52 @@ window.onload = async function () {
 };
 
 async function readRankOp() {
-  fetch("http://127.0.0.1:3000/agents/:pontos_gamificacao/ranking", {
-    headers: { "Content-Type": "application/json" },
-    method: "GET",
-  })
+  let table = $("#tabela-ranking-operacionais").DataTable();
+
+  fetch("http://127.0.0.1:3000/agents/:pontos_gamificacao/ranking")
     .then((res) => res.json())
     .then((out) => {
-      $("#tabela-ranking-operacionais tbody").empty();
-      $.each(out, function (index, valor) {
-        $("#tabela-ranking-operacionais tbody").append(
-          "<tr>" +
-            "<td>" +
-            valor.Ranking_operacionais +
-            "</td>" +
-            "<td>" +
-            valor.pontos_gamificacao +
-            "</td>" +
-            "<td>" +
-            valor.id_operacional +
-            "</td>" +
-            "</tr>"
-        );
+      $.each(out, function (index, value) {
+        table.row
+          .add([
+            value.Ranking_operacionais,
+            value.pontos_gamificacao,
+            value.username,
+            value.id_cargo,
+          ])
+          .draw();
       });
     })
-    .catch((err) => {
-      alert("Erro ao carregar a tabela!" + err);
-    });
+    .catch((err) => console.error(err));
 }
 
-async function readRankEq() {
-  fetch("http://127.0.0.1:3000/teams/:creditos_equipa/ranking", {
-    headers: { "Content-Type": "application/json" },
-    method: "GET",
-  })
+$(document).ready(function () {
+  $("#tabela-ranking-operacionais").DataTable();
+  readRankOp();
+});
+
+function readRankEq() {
+  let table = $("#tabela-ranking-equipa").DataTable();
+
+  fetch("http://127.0.0.1:3000/teams/:creditos_equipa/ranking")
     .then((res) => res.json())
     .then((out) => {
-      $("#tabela-ranking-equipa tbody").empty();
-      $.each(out, function (index, valor) {
-        $("#tabela-ranking-equipa tbody").append(
-          "<tr>" +
-            "<td>" +
-            valor.Ranking_equipa +
-            "</td>" +
-            "<td>" +
-            valor.creditos_equipa +
-            "</td>" +
-            "<td>" +
-            valor.id_equipa +
-            "</td>" +
-            "</tr>"
-        );
+      $.each(out, function (index, value) {
+        table.row
+          .add([
+            value.Ranking_equipa,
+            value.creditos_equipa,
+            value.id_equipa,
+            "filler",
+            "filler",
+          ])
+          .draw();
       });
     })
-    .catch((err) => {
-      alert("Erro ao carregar a tabela!" + err);
-    });
+    .catch((err) => console.error(err));
 }
+
+$(document).ready(function () {
+  $("#tabela-ranking-equipa").DataTable();
+  readRankEq();
+});
