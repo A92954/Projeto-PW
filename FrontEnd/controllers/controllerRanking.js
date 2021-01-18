4332841bc92f033fe1b321f22a6e0653f8527012
A92954 <a92954@alunos.uminho.pt>
2021-01-18 16:23:28 +0000

    rankingv2

--- FrontEnd/controllers/controllerRanking.js
+++ FrontEnd/controllers/controllerRanking.js
@@ -1,8 +1,9 @@
 window.onload = async function () {
+  readRankEq();
   readRankOp();
 };
 
-function readRankOp() {
+async function readRankOp() {
   fetch("http://127.0.0.1:3000/agents/:pontos_gamificacao/ranking", {
     headers: { "Content-Type": "application/json" },
     method: "GET",
@@ -21,6 +22,35 @@ function readRankOp() {
             "</td>" +
             "<td>" +
             valor.id_operacional +
+            "</td>" +
+            "</tr>"
+        );
+      });
+    })
+    .catch((err) => {
+      alert("Erro ao carregar a tabela!" + err);
+    });
+}
+
+async function readRankEq() {
+  fetch("http://127.0.0.1:3000/teams/:creditos_equipa/ranking", {
+    headers: { "Content-Type": "application/json" },
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((out) => {
+      $("#tabela-ranking-equipa").empty();
+      $.each(out, function (index, valor) {
+        $("#tabela-ranking-equipa").append(
+          "<tr>" +
+            "<td>" +
+            valor.Ranking_equipa +
+            "</td>" +
+            "<td>" +
+            valor.creditos_equipa +
+            "</td>" +
+            "<td>" +
+            valor.id_equipa +
             "</td>" +
             "</tr>"
         );
