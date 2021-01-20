4f9bcda73699a921e0444bb180083ba77d590a62
A92954 <a92954@alunos.uminho.pt>
2021-01-20 23:24:37 +0000

    l

--- FrontEnd/controllers/controllerOperacional.js
+++ FrontEnd/controllers/controllerOperacional.js
@@ -6,7 +6,6 @@ function ocorrAtual() {
   var idEq = 6;
   var response = await fetch(`http://127.0.0.1:3000/teams/${idEq}/view_team`);
   var nomeEq = response.nome_equipa;
-  var Eq = JSON.stringify(nomeEq);
-  $("#nomeEquipaAtual").text(nomeEq);
+  document.getElementById("nomeEquipaAtual").innerHTML
  
 }
