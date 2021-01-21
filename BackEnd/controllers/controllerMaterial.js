a16ebc96dc78926b56277df840a499003fb450d7
RicsdaFazers <a92941@alunos.uminho.pt>
2021-01-21 00:12:25 +0000

    mensagens de erro

--- BackEnd/controllers/controllerMaterial.js
+++ BackEnd/controllers/controllerMaterial.js
@@ -1,13 +1,18 @@
 const connect = require("../database");
 
 function read(req, res) {
-  const query = connect.con.query(
-    "SELECT * FROM material",
+  const query = connect.con.query("SELECT * FROM material",
     function (err, rows, fields) {
-      if (err) return res.status(500).end();
-      res.send(rows);
-    }
-  );
+      if (!err) {
+        if (rows.length == 0) {
+          res.status(404).send("Data not found");
+        }
+        else {
+          res.status(200).send(rows);
+        }
+      } else
+        console.log('Error while performing Query.', err);
+  });
 }
 
 /*function confirmarLevantamento(req, res) {
@@ -39,9 +44,17 @@ function readMaterialOcorrencia(req, res) {
   const id_ocorrencia = req.params.id_ocorrencia;
   const query = connect.con.query('SELECT m.nome_material, om.quantidade_usada FROM material m, ocorrencia_material om WHERE om.id_ocorrencia = ? and m.id_material = om.id_material', id_ocorrencia,
     function(err, rows, fields) {
-      if (err) return res.status(500).end();
-      res.send(rows);
-    });
+      if (!err) {
+        if (rows.length == 0) {
+          res.status(404).send({"msg": "data not found"});
+        }
+        else {
+          res.status(200).send(rows);
+        }
+      } else
+        res.status(400).send({"msg": err.code});
+        console.log('Error while performing Query.', err);
+  });
 }
 
 function readConfirmarMaterialUsado(req, res) {
@@ -50,8 +63,16 @@ function readConfirmarMaterialUsado(req, res) {
     "SELECT m.nome_material, om.quantidade_usada FROM ocorrencia_material om, material m WHERE m.id_material = ? and om.id_material = m.id_material",
     id_material,
     function (err, rows, fields) {
-      if (err) return res.status(500).end();
-      res.send(rows[0]);
+      if (!err) {
+        if (rows.length == 0) {
+          res.status(404).send({"msg": "data not found"});
+        }
+        else {
+          res.status(200).send(rows[0]);
+        }
+      } else
+        res.status(400).send({"msg": err.code});
+        console.log('Error while performing Query.', err);
     }
   );
 }
