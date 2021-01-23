98d9ba6b2e44db21927bfb1a6f7574de379cb7c5
Mariana <a92986@alunos.uminho.pt>
2021-01-23 21:05:19 +0000

    Tudo direito amém (com PWA)

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,6 @@
+const express = require("express");
 
-const express = require('express');
-
-const hostname = process.env.HOST || '127.0.0.1';
+const hostname = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3000;
 
 //carregar bibliotecas
@@ -9,13 +8,13 @@ const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const session = require("express-session");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-const expressSanitizer = require('express-sanitizer');
+const bodyParser = require("body-parser");
+const expressSanitizer = require("express-sanitizer");
 //const expressValidator = require('express-validator');
-const { body } = require('express-validator');
+const { body } = require("express-validator");
 
 //iniciar aplicação
-const app = express(); 
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,20 +23,20 @@ app.use(expressSanitizer());
 app.use(express.json());
 
 //forçar uso de bibliotecas
-app.use(cors({
-    exposedHeaders: ['Location'],
-}));
+app.use(
+  cors({
+    exposedHeaders: ["Location"],
+  })
+);
 
-
-app.use('/assets', express.static('assets'));
-app.use('/views', express.static('views'));
-app.listen(port, function(err){
-    if (!err) {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    }
-    else {
-        console.log(err);
-    }
+app.use("/assets", express.static("assets"));
+app.use("/views", express.static("views"));
+app.listen(port, function (err) {
+  if (!err) {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  } else {
+    console.log(err);
+  }
 });
 
 /* //LOGIN
@@ -58,4 +57,4 @@ passport.authenticate('local', {successRedirect: '/',
 
 // criar um loader
 module.exports = app;
-require('./loader.js'); 
+require("./loader.js");
