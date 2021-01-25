cfc313b4005adc322909a91897bc1250d9c2abac
A92954 <a92954@alunos.uminho.pt>
2021-01-25 02:38:02 +0000

    hgfh

--- FrontEnd/controllers/controllerConfirmarOcorrencia.js
+++ FrontEnd/controllers/controllerConfirmarOcorrencia.js
@@ -1,121 +1,128 @@
 //const { updateDuracaoOcorrencia } = require("../../BackEnd/controllers/controllerOcorrencia");
-
-document.getElementById("btn_ConcluirOperacao").onclick = function () {
-  update_DataFim();
+window.onload = function () {
+  let user = localStorage.User;
+  getIdOp(user);
 };
 
-var id_ocorr = "4";
-
-
-function update_DataFim() {
-    console.log(id_ocorr);
-    fetch(`http://127.0.0.1:3000/occurrences/${id_ocorr}/finishdate`, {
-      //mudar a rota do fetch
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+function getIdOp(ler) {
+  fetch(`http://127.0.0.1:3000/users/${ler}/info`)
+    .then((res) => res.json())
+    .then((out) => {
+      let id_oper = out[0].id_operacional;
+      getOcorr(id_oper);
     })
-      .then((res) => res.text())
-      .then((out) => {
-        alert("1");
-        update_PercentagemSobreviventes();
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
+    .catch((err) => console.error(err));
+}
 
-
-  function update_PercentagemSobreviventes() {
-    console.log(id_ocorr);
-    fetch(`http://127.0.0.1:3000/occurrences/${id_ocorr}/survival`, {
-      //mudar a rota do fetch
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+function getOcorr(id_op) {
+  fetch(`http://127.0.0.1:3000/occurrences/${id_op}/accurring`)
+    .then((res) => res.json())
+    .then((out) => {
+      id_ocorr = out[0].id_ocorrencia;
+      document.getElementById("btn_ConcluirOperacao").onclick = function () {
+        update_DataFim(id_ocorr);
+      };
     })
-      .then((res) => res.text())
-      .then((out) => {
-       alert("2");
-        update_DuracaoOcorrencia();
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
+    .catch((err) => console.error(err));
+}
 
-
-  function update_DuracaoOcorrencia() {
-    console.log(id_ocorr);
-    fetch(`http://127.0.0.1:3000/occurrences/${id_ocorr}/duration`, {
-      //mudar a rota do fetch
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+function update_DataFim(ler) {
+  fetch(`http://127.0.0.1:3000/occurrences/${ler}/finishdate`, {
+    //mudar a rota do fetch
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((out) => {
+      alert("1");
+      update_PercentagemSobreviventes(ler);
     })
-      .then((res) => res.text())
-      .then((out) => {
-       alert("3");
-        update_CreditoOcorrencia();
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
+    .catch((error) => {
+      alert(error);
+    });
+}
 
-
-  function update_CreditoOcorrencia() {
-    console.log(id_ocorr);
-    fetch(`http://127.0.0.1:3000/occurrences/${id_ocorr}/credit`, {
-      //mudar a rota do fetch
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+function update_PercentagemSobreviventes(ler) {
+  fetch(`http://127.0.0.1:3000/occurrences/${ler}/survival`, {
+    //mudar a rota do fetch
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((out) => {
+      alert("2");
+      update_DuracaoOcorrencia(ler);
     })
-      .then((res) => res.text())
-      .then((out) => {
-       alert("4");
-        update_CreditoEquipa();
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
+    .catch((error) => {
+      alert(error);
+    });
+}
 
-
-  function update_CreditoEquipa() {
-    console.log(id_ocorr);
-    fetch(`http://127.0.0.1:3000/occurrences/${id_ocorr}/credit_team`, {
-      //mudar a rota do fetch
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+function update_DuracaoOcorrencia(ler) {
+  fetch(`http://127.0.0.1:3000/occurrences/${ler}/duration`, {
+    //mudar a rota do fetch
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((out) => {
+      alert("3");
+      update_CreditoOcorrencia(ler);
     })
-      .then((res) => res.text())
-      .then((out) => {
-       alert("5");
-        update_CreditoOperacional();
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
+    .catch((error) => {
+      alert(error);
+    });
+}
 
-
-  function update_CreditoOperacional() {
-    console.log(id_ocorr);
-    fetch(`http://127.0.0.1:3000/occurrences/${id_ocorr}/put_credit`, {
-      //mudar a rota do fetch
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+function update_CreditoOcorrencia(ler) {
+  fetch(`http://127.0.0.1:3000/occurrences/${ler}/credit`, {
+    //mudar a rota do fetch
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((out) => {
+      alert("4");
+      update_CreditoEquipa(ler);
     })
-      .then((res) => res.text())
-      .then((out) => {
-       alert("6");
-        
-      })
-      .catch((error) => {
-        alert(error);
-      });
-  }
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function update_CreditoEquipa(ler) {
+  fetch(`http://127.0.0.1:3000/occurrences/${ler}/credit_team`, {
+    //mudar a rota do fetch
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((out) => {
+      alert("5");
+      update_CreditoOperacional(ler);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
+
+function update_CreditoOperacional(ler) {
+  fetch(`http://127.0.0.1:3000/occurrences/${ler}/put_credit`, {
+    //mudar a rota do fetch
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.text())
+    .then((out) => {
+      alert("6");
+    })
+    .catch((error) => {
+      alert(error);
+    });
+}
 
 //mostrar materiais no relatorio
-function enviaDados() {   
+function enviaDados(ler) {
   fetch(`http://127.0.0.1:3000/occurrences/${ler}/sendmail`, {
     //mudar a rota do fetch
     headers: { "Content-Type": "application/json" },
@@ -123,8 +130,6 @@ function enviaDados() {
   })
     .then((res) => res.json())
     .then((out) => {
-     alert("7");
-      });
-    
+      alert("7");
+    });
 }
- 
