e1cf6874e3cdfb70a7ab9d5a49e0a7692191a46b
Filipe Araújo <filipe@MacBook-Pro-de-Filipe.local>
2021-01-25 16:20:38 +0000

    .

--- FrontEnd/controllers/controllerOcorrencia.js
+++ FrontEnd/controllers/controllerOcorrencia.js
@@ -65,7 +65,8 @@ function confirmarOcorrencia() {
     .then((res) => res.text())
     .then((out) => {
       alert(out);
-      window.location.href = "http://127.0.0.1:5502/FrontEnd/Relatorio.html";
+      confirmarMaterial();
+      //window.location.href = "http://127.0.0.1:5502/FrontEnd/Relatorio.html";
     })
     .catch((error) => {
       alert(error);
