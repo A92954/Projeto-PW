f31b98b7b2a40fa4301d9256d6d79329340c40aa
RicsdaFazers <a92941@alunos.uminho.pt>
2021-01-25 01:48:07 +0000

    asd

--- BackEnd/controllers/controllerEquipa.js
+++ BackEnd/controllers/controllerEquipa.js
@@ -153,5 +153,5 @@ module.exports = {
   readRankingEquipa: readRankingEquipa,
   readMembrosEquipa: readMembrosEquipa,
   updateConfirmarEquipa: updateConfirmarEquipa,
-  //updateCreditoEquipa: updateCreditoEquipa
+  updateCreditoEquipa: updateCreditoEquipa
 }
