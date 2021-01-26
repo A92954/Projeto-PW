d8c22051b1d8fc6981b90038cbeaebeea04029ac
A92954 <a92954@alunos.uminho.pt>
2021-01-26 18:06:57 +0000

    cvb

--- BackEnd/controllers/controllerMaterial.js
+++ BackEnd/controllers/controllerMaterial.js
@@ -2,19 +2,18 @@ const connect = require("../database");
 
 //Funcao que le todos os dados referentes aos materiais
 function read(req, res) {
-  const query = connect.con.query('SELECT * FROM material',
-  function (err, rows, fields) {
-    if (!err) {
-      if (rows.length == 0) {
-        res.status(404).send({ msg: "Data not found" });
-      }
-      else {
-        res.status(200).send(rows);
-      }
+  const query = connect.con.query(
+    "SELECT * FROM material",
+    function (err, rows, fields) {
+      if (!err) {
+        if (rows.length == 0) {
+          res.status(404).send({ msg: "Data not found" });
+        } else {
+          res.status(200).send(rows);
+        }
+      } else console.log("Error while performing Query.", err);
     }
-    else
-      console.log('Error while performing Query.', err);
-  });
+  );
 }
 
 /*function confirmarLevantamento(req, res) {
@@ -45,68 +44,80 @@ function read(req, res) {
 //Funcao que le os materiais disponibilizados para uma determinada ocorrencia, bem como a sua quantidade
 function readMaterialOcorrencia(req, res) {
   const id_ocorrencia = req.params.id_ocorrencia;
-  const query = connect.con.query('SELECT m.nome_material, om.quantidade_usada FROM material m, ocorrencia_material om WHERE om.id_ocorrencia = ? and m.id_material = om.id_material', id_ocorrencia,
-  function(err, rows, fields) {
-    if (!err) {
-      if (rows.length == 0) {
-        res.status(404).send({ msg: "Data not found" });
-      }
-      else {
-        res.status(200).send(rows);
+  const query = connect.con.query(
+    "SELECT m.nome_material, om.quantidade_usada FROM material m, ocorrencia_material om WHERE om.id_ocorrencia = ? and m.id_material = om.id_material",
+    id_ocorrencia,
+    function (err, rows, fields) {
+      if (!err) {
+        if (rows.length == 0) {
+          res.status(404).send({ msg: "Data not found" });
+        } else {
+          res.status(200).send(rows);
+        }
+      } else {
+        res.status(400).send({ msg: err.code });
+        console.log("Error while performing Query.", err);
       }
     }
-    else {
-      res.status(400).send({ msg: err.code});
-      console.log('Error while performing Query.', err);
-    }
-  });
+  );
 }
 
 //Funcao que le apenas os materiais que foram confirmados para irem a uma determinada ocorrencia
 function readConfirmarMaterialUsado(req, res) {
   const id_material = req.params.id_material;
-  const query = connect.con.query('SELECT m.nome_material, om.quantidade_usada FROM ocorrencia_material om, material m WHERE m.id_material = ? and om.id_material = m.id_material and om.confirmado_material = 1', id_material,
-  function (err, rows, fields) {
-    if (!err) {
-      if (rows.length == 0) {
-        res.status(404).send({ msg: "Data not found"});
-      }
-      else {
-        res.status(200).send(rows[0]);
+  const query = connect.con.query(
+    "SELECT m.nome_material, om.quantidade_usada FROM ocorrencia_material om, material m WHERE m.id_material = ? and om.id_material = m.id_material and om.confirmado_material = 1",
+    id_material,
+    function (err, rows, fields) {
+      if (!err) {
+        if (rows.length == 0) {
+          res.status(404).send({ msg: "Data not found" });
+        } else {
+          res.status(200).send(rows[0]);
+        }
+      } else {
+        res.status(400).send({ msg: err.code });
+        console.log("Error while performing Query.", err);
       }
     }
-    else {
-      res.status(400).send({"msg": err.code});
-      console.log('Error while performing Query.', err);
-    }
-  });
+  );
 }
 
 //Funcao que le um id_ocorrencia e altera o booleano, default false, para true, para confirmar os materiais da mesma
 function updateConfirmarLevantamento(req, res) {
   const id_ocorrencia = req.params.id_ocorrencia;
   let id_material;
-  const query = connect.con.query('SELECT id_material FROM ocorrencia_material WHERE id_ocorrencia = ?', id_ocorrencia,
-  function(err, rows, fields) {
-    console.log(rows);
-    for(let i = 0; i < rows.length; i++) {
-      id_material = rows[i].id_material;
-      const update1 = [id_material, id_ocorrencia];
-      const secondquery = connect.con.query('UPDATE ocorrencia_material SET confirmado_material = 1 WHERE id_material = ? AND id_ocorrencia = ?', update1,
-      function(err, rows, fields) {
-        console.log(secondquery.sql);
-        if(err) {
-          res.status(400).send({ msg: err.code });
-        }
-        else res.send("Os materiais da ocorrencia " +id_ocorrencia+ " foram confirmados");
-      });
+  const query = connect.con.query(
+    "SELECT id_material FROM ocorrencia_material WHERE id_ocorrencia = ?",
+    id_ocorrencia,
+    function (err, rows, fields) {
+      console.log(rows);
+      for (let i = 0; i > rows.length; i++) {
+        id_material = rows[i].id_material;
+        const update1 = [id_material, id_ocorrencia];
+        const secondquery = connect.con.query(
+          "UPDATE ocorrencia_material SET confirmado_material = 1 WHERE id_material = ? AND id_ocorrencia = ?",
+          update1,
+          function (err, rows, fields) {
+            console.log(secondquery.sql);
+            if (err) {
+              res.status(400).send({ msg: err.code });
+            } else
+              res.send(
+                "Os materiais da ocorrencia " +
+                  id_ocorrencia +
+                  " foram confirmados"
+              );
+          }
+        );
+      }
     }
-  });
+  );
 }
 
 module.exports = {
   read: read,
   readMaterialOcorrencia: readMaterialOcorrencia,
   readConfirmarMaterialUsado: readConfirmarMaterialUsado,
-  updateConfirmarLevantamento: updateConfirmarLevantamento
-}
+  updateConfirmarLevantamento: updateConfirmarLevantamento,
+};
